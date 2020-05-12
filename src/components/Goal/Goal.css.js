a40ebbac9522593d68f32909e96dc62831823e5b
Hubert Wojcik <hwojcikk@gmail.com>
2020-05-12 21:17:20 +0200

    create basic structure of set goal screen

--- src/components/Goal/Goal.css.js
+++ src/components/Goal/Goal.css.js
@@ -13,14 +13,59 @@ export const Title = styled.div`
 	text-align: center;
 	border-bottom: 1px solid gold;
 	padding: 30px;
+	font-weight: bold;
 `;
-export const GoalContainer = styled.div`
+export const GoalContent = styled.div`
 	display: flex;
+	flex-direction: column;
+
 	justify-content: space-around;
 	flex-wrap: wrap;
-	background-color: red;
 	align-items: center;
 	flex: 1;
 	width: 100%;
 	margin-bottom: 20px;
+`;
+
+export const GoalTitle = styled.h3`
+	color: black;
+`;
+
+export const GoalValue = styled.div`
+	width: 50px;
+	margin-left: 30px;
+	border: 1px solid black;
+	padding: 10px 40px;
+	border-radius: 5px;
+	background-color: #ddd;
+`;
+
+export const GoalContainer = styled.div`
+	display: flex;
+	align-items: center;
+	color: black;
+	width: 50%;
+	height: 50px;
+	.slider {
+		-webkit-appearance: none;
+		width: 100%;
+		height: 10px;
+		border-radius: 15px;
+		background: #67e7ec;
+		outline: none;
+		&::-webkit-slider-thumb {
+			-webkit-appearance: none;
+			appearance: none;
+			width: 25px;
+			height: 25px;
+			border-radius: 50px;
+			background: #045bf5;
+		}
+		&::-moz-range-thumb {
+			width: 25px;
+			height: 25px;
+			border-radius: 50px;
+			background: #045bf5;
+		}
+	}
 `;
--- src/components/Goal/Goal.js
+++ src/components/Goal/Goal.js
@@ -1,11 +1,36 @@
-import React from "react";
-import { Container, Title, GoalContainer } from "./Goal.css";
+import * as React from "react";
+import {
+	Container,
+	Title,
+	GoalContent,
+	GoalContainer,
+	GoalTitle,
+	GoalValue,
+} from "./Goal.css";
+import { Button } from "../BookForm/BookForm.css";
 
 const Goal = () => {
+	const [readGoal, setReadGoal] = React.useState(0);
+
+	const handleOnChange = (e) => setReadGoal(e.target.value);
 	return (
 		<Container>
 			<Title>Set your goal!</Title>
-			<GoalContainer />
+			<GoalContent>
+				<GoalTitle>Set number of books You'd like to read this year</GoalTitle>
+				<GoalContainer>
+					<input
+						type="range"
+						min={0}
+						max={200}
+						value={readGoal}
+						onChange={handleOnChange}
+						className="slider"
+					/>
+					<GoalValue>{readGoal}</GoalValue>
+				</GoalContainer>
+				<Button>Set Goal!</Button>
+			</GoalContent>
 		</Container>
 	);
 };
