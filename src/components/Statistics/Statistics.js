e99bea137d25f8d6dbd7f3b906ff9554cd08fef9
Hubert Wojcik <hwojcikk@gmail.com>
2020-05-18 22:13:40 +0200

    create basic statisctics screen

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,17 +1,40 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Chart from "../Chart/Chart";
+import {
+	StatsHeader,
+	StatsContainer,
+	ProgressContainer,
+	Container,
+} from "./Statistics.css";
+import ProgressBar from "./ProgressBar/ProgressBar";
 
 const Statistics = () => {
 	const totalNumberOfPages = useSelector((state) => state.books.totalPages);
-	const NumberOfDaysToFinish = useSelector(
-		(state) => state.books.books.daysToFinish
+	const numberOfBooks = useSelector(
+		(state) => state.books.finishedBooks.length
 	);
+	console.log(numberOfBooks);
+	const setGoal = useSelector((state) => state.goal.booksGoal);
+
+	const [percentages, setPercentages] = React.useState(0);
+
+	React.useEffect(() => {
+		setPercentages(((numberOfBooks / setGoal) * 100).toFixed(1));
+	}, [numberOfBooks, setGoal]);
 
 	return (
 		<>
-			<div> Total number of Pages : {totalNumberOfPages}</div>
-			<Chart />
+			<Container>
+				<StatsHeader>Total number of Pages : {totalNumberOfPages}</StatsHeader>
+				<StatsContainer>
+					<ProgressContainer>
+						<h2>Your current goal progress</h2>
+						<ProgressBar percentageOfGoal={percentages} />
+					</ProgressContainer>
+					<div>q</div>
+				</StatsContainer>
+			</Container>
 		</>
 	);
 };
