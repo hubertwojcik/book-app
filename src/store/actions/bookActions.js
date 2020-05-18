0d5656bc578b1cee4382ef80dcc3d5cae792f0da
Hubert Wojcik <hwojcikk@gmail.com>
2020-05-18 22:13:22 +0200

    create action for adding currently read books

--- src/store/actions/bookActions.js
+++ src/store/actions/bookActions.js
@@ -1,8 +1,15 @@
-export const ADD_BOOK = "ADD_BOOK";
+export const ADD_FINISHED_BOOK = "ADD_FINISHED_BOOK";
+export const ADD_CURRENT_BOOK = "ADD_CURRENT_BOOK";
 
-export const addBook = (bookDetails) => {
+export const addFinishedBook = (bookDetails) => {
 	return {
-		type: ADD_BOOK,
+		type: ADD_FINISHED_BOOK,
+		payload: bookDetails,
+	};
+};
+export const addCurrentBook = (bookDetails) => {
+	return {
+		type: ADD_CURRENT_BOOK,
 		payload: bookDetails,
 	};
 };
