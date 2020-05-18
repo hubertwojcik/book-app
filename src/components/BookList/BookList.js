d6ec9078f11c5489de3a4dc4e2b42fa36c6efdd0
Hubert Wojcik <hwojcikk@gmail.com>
2020-05-18 22:10:52 +0200

    add distinction to BookList in order if book is finished

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,27 +1,78 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Route, Link } from "react-router-dom";
+
 import { Container, Title, ListContainer, ListItem } from "./BookList.css";
 import BookItem from "./BookItem/BookItem";
 
 const BookList = () => {
-	const books = useSelector((state) => state.books.books);
+	const finishedBooks = useSelector((state) => state.books.finishedBooks);
+	const currBooks = useSelector((state) => state.books.currBooks);
+
+	const allBooks = finishedBooks.concat(currBooks);
 
 	return (
 		<Container>
 			<Title>Book list!</Title>
+			<Link to="/list">All Books</Link>
+			<Link to="/list/finished">Finished Books</Link>
+			<Link to="/list/reading">Currently Reading</Link>
+
 			<ListContainer>
-				{books.length !== 0 &&
-					books.map((item) => (
-						<BookItem
-							author={item.author}
-							title={item.title}
-							isbn={item.isbn}
-							numOfPages={item.numOfPages}
-							startDate={item.startDate}
-							endDate={item.endDate}
-							daysToFinish={item.daysToFinish}
-						/>
-					))}
+				<Route path="/list" exact>
+					{allBooks.length !== 0 &&
+						allBooks.map((item) =>
+							item.hasOwnProperty("currPage") ? (
+								<BookItem
+									author={item.author}
+									title={item.title}
+									isbn={item.isbn}
+									numOfPages={item.numOfPages}
+									startDate={item.startDate}
+									currentPage={item.currPage}
+								/>
+							) : (
+								<BookItem
+									finished
+									author={item.author}
+									title={item.title}
+									isbn={item.isbn}
+									numOfPages={item.numOfPages}
+									startDate={item.startDate}
+									endDate={item.endDate}
+									daysToFinish={item.daysToFinish}
+								/>
+							)
+						)}
+				</Route>
+				<Route path="/list/finished">
+					{finishedBooks.length !== 0 &&
+						finishedBooks.map((item) => (
+							<BookItem
+								finished
+								author={item.author}
+								title={item.title}
+								isbn={item.isbn}
+								numOfPages={item.numOfPages}
+								startDate={item.startDate}
+								endDate={item.endDate}
+								daysToFinish={item.daysToFinish}
+							/>
+						))}
+				</Route>
+				<Route path="/list/reading">
+					{currBooks.length !== 0 &&
+						currBooks.map((item) => (
+							<BookItem
+								author={item.author}
+								title={item.title}
+								isbn={item.isbn}
+								numOfPages={item.numOfPages}
+								startDate={item.startDate}
+								currentPage={item.currPage}
+							/>
+						))}
+				</Route>
 			</ListContainer>
 		</Container>
 	);
