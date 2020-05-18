0e95b2f2178014dda8516d4bc6ffd61b9734c2ec
Hubert Wojcik <hwojcikk@gmail.com>
2020-05-18 22:07:33 +0200

    create input component

---
+++ src/components/UI/Input/Input.css.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+	width: 300px;
+	height: 35px;
+	border: 1px solid #ccc;
+	background-color: #fff;
+`;
+export const Label = styled.label`
+	font-size: 16px;
+`;
