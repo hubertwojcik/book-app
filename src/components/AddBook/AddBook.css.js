c80f211ee08a4508941963cdd309466adab9c35e
Hubert Wojcik <hwojcikk@gmail.com>
2020-05-18 22:11:14 +0200

    create styled components

---
+++ src/components/AddBook/AddBook.css.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import Modal from "styled-react-modal";
+
+export const StyledModal = Modal.styled`
+display: flex;
+ width: 60%;
+  height:80%;
+  align-items: center;
+  justify-content: center;
+  background-color:white;;
+`;
+
+export const StyledForm = styled.form`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+	width: 60%;
+	height: 60%;
+	align-self: center;
+	background-color: red;
+`;
